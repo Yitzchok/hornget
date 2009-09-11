1503eeb0084541c5972c5eedd6c34e6ce24b99ac
dagda1@scotalt.net <dagda1@scotalt.net@63161db4-6c6d-11de-8599-9336f0829f90>
2009-09-11 21:19:00 +0000

    Set up of horn.services branch.

--- branches/horn.services/src/horn.services/horn.services.core.tests.unit/parser.specs/parserspecs.cs
+++ branches/horn.services/src/horn.services/horn.services.core.tests.unit/parser.specs/parserspecs.cs
@@ -1,11 +1,23 @@
 using System;
+using Xunit;
 
 namespace horn.services.core.tests.unit.parser.specs
 {
-    public class When_traversing_the_package_tree : Specification 
+    public class When_horn_scans_one_package_folder : Specification 
     {
         protected override void Because()
         {            
+        }
+
+        [Fact]
+        public void Then_the_package_meta_is_recorded()
+        {
+            
+        }
+
+        protected override void After_each_spec()
+        {
+            
         }
     }
 }
